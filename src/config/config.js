32104d1968e1bb22d0f9c133833091758b2026ca
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-04-30 19:09:53 +0530

    - added dotenv

--- src/config/config.js
+++ src/config/config.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBlJSki1LhkzIU8oD52f1HkoqPWVDzcCuo",
-    authDomain: "covizone-9c238.firebaseapp.com",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
     databaseURL: "https://covizone-9c238-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "covizone-9c238",
-    storageBucket: "covizone-9c238.appspot.com",
-    messagingSenderId: "469080536924",
-    appId: "1:469080536924:web:6a2818b6fa80558e4a7399",
-    measurementId: "G-TG200JV1YW"
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 }
 const app = initializeApp(firebaseConfig);
 
